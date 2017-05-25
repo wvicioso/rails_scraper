caddd8a776ab86087858eeace56932985dfb51ba
Wellington Vicioso <wellington.vicioso@gmail.com>
2017-05-25 01:52:21 -0400

    create tests

---
+++ test/controllers/websites_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class WebsitesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @website = websites(:one)
+  end
+
+  test "should get index" do
+    get websites_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_website_url
+    assert_response :success
+  end
+
+  test "should create website" do
+    assert_difference('Website.count') do
+      post websites_url, params: { link: 'https://stackoverflow.com/questions/44173150/progressdialog-showing-white-background-dialog-with-progress-inside-it' }
+    end
+
+    assert_redirected_to website_url(Website.last)
+  end
+
+  test "should show website" do
+    get website_url(@website)
+    assert_response :success
+  end
+
+end
---
+++ test/models/html_tag_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HtmlTagTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
